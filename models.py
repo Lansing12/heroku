185b206ce1844f6ecdf8664262cb6a5cc583ce96
Lansing12 <benediktas.gilis@gmail.com>
2019-05-07 18:49:12 +0300

    abc

---
+++ models.py
@@ -0,0 +1,8 @@
+from smartninja_nosql.odm import Model
+
+
+class Secret_Number(Model):
+    def__init__(self, name, number, **kwargs):
+        self.name = name
+        self.secret_number = secret_number
+        super().__init__(**kwargs)
